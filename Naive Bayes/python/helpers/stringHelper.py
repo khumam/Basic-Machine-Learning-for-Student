e98b7e23cf7c0be9a3b5c2306fb8aa991a879d46
Khoerul Umam <khoerulumam@students.unnes.ac.id>
2022-03-21 07:53:29 +0700

    [UPDATE] Singkatan

--- Naive Bayes/python/helpers/stringHelper.py
+++ Naive Bayes/python/helpers/stringHelper.py
@@ -1,8 +1,20 @@
+import re
 from Sastrawi.Stemmer.StemmerFactory import StemmerFactory
+from Sastrawi.StopWordRemover.StopWordRemoverFactory import StopWordRemoverFactory
 factory = StemmerFactory()
 stemmer = factory.create_stemmer()
+removeFactory = StopWordRemoverFactory()
+stopword = removeFactory.create_stop_word_remover()
 
 
 def stemString(string):
     output = stemmer.stem(string)
     return output
+
+
+def removeStopWord(string):
+    return stopword.remove(string)
+
+
+def cleanString(string):
+    return re.sub('[\W_]|(URL)|(USERNAME)', ' ', string)
